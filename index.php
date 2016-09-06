f7a1b5e13284fa5bff95e7dd27d85bd77890c76b
gregcharest <greg.charest@gmail.com>
2016-09-06 12:34:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 <html>
   <link rel="stylesheet" href="css/main.css" type="text/css">
 <body>
-<p id="header">Partners Health Care</p>
 <?php
 echo "<p>";
-echo "<h2>TSO's Strategy Leadership Meeting Website<h2>";
+echo "<p id="header">Partners Health Care</p>";
+echo "<h2>TSO's Strategy Leadership Meeting Website</h2>";
 echo "
+<h3>
 <ul>
 <li>Announcements</li>
 <li>Container Project</li>
 <li>Cloud Update</li>
 </ul>
+</h3>
 ";
 
 ?>
