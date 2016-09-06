4de0b694c9140086cac801113bff06ef648d6e8c
gregcharest <greg.charest@gmail.com>
2016-09-06 12:22:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 <?php
 echo "<p>";
 echo "<h2>TSO's Strategy Leadership Meeting Website<h2>";
+echo "
+<ul>
+<li>Announcements</li>
+<li>Container Project</li>
+<li>Cloud Update</li>
+</ul>
+";
+
 ?>
 
 </body>
